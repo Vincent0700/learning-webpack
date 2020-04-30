b08c18e60e9a85380341353c52be9cabda39003d
vincent0700 <wang.yuanqiu007@gmail.com>
2020-04-30 18:56:39 +0800

    upd: update utils.js

--- src/templates/utils.js
+++ src/templates/utils.js
@@ -1,11 +1,9 @@
 export const add = (x, y) => x + y;
-export const minus = (x, y) => x - y;
-export const multiply = (x, y) => x * y;
-export const divide = (x, y) => x / y;
+export const num = 10;
+export const obj = { a: { b: 1 } };
 
 export default {
   add,
-  minus,
-  multiply,
-  divide
+  num,
+  obj
 };
